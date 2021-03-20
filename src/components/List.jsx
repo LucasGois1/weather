6c5da52eaffa78b5ce7a91e26201d44c7de606ec
Lucas Sampaio de Góis <lucassampaiodegois1@gmail.com>
2021-03-20 02:44:39 -0300

    v1.2

---
+++ src/components/List.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './List.css'
+
+const List = (props) => {
+    const data = props.list
+    return (
+        <div className="list-area">
+            {data[0].map((item, key) => {
+                return (
+                    <div className="list-item" key={key}>
+                        <div className="item-image">
+                            <img src={`assets/icons/png/0.png`} alt="weather" />
+                        </div>
+                        <div className="item-temp-area">
+                            <h4>{`${item.max}º/`}</h4>
+                            <h4>{`${item.min}º`}</h4>
+                        </div>
+                        <div className="item-data">
+                            <h4>{item.weekday}</h4>
+                            <h5>{item.date}</h5>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+export default List
