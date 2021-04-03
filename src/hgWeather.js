3a5dff84282d0d98ccd6d45534ac0b99533bb625
Lucas Gois <lucassampaiodegois1@gmail.com>
2021-04-03 11:16:06 -0300

    api key security update

--- src/hgWeather.js
+++ src/hgWeather.js
@@ -1,6 +1,6 @@
 //salvando em constantes a minha senha da api, e o endereco de busca padrao
 const API_BASE = 'https://api.hgbrasil.com/weather'
-const API_KEY = 'a8ceed66'
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 
 // funcao basica de consultas
